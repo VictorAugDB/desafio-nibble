f7ed3649637f811ac39f4f1be915166223cd1886
VictorAugDB <vdbernardes@gmail.com>
2021-04-25 19:27:34 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import 'reflect-metadata';
 import './database';
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
